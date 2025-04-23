6602b4d7071cafa18c0772a039353e78ad851935
andre <andre.monteiro@gazin.com.br>
2025-04-23 17:48:30 -0300

    feat: update github pages workflow

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,11 @@ const config = {
 
   kit: {
     adapter: adapter({
-      fallback: "index.html",
-    }),
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
   },
 };
 
